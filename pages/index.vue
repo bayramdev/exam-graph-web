b79045752d047dd73df62a117f32c752a3b64576
bayramdev <48856944+bayramdev@users.noreply.github.com>
2022-04-27 23:20:24 +0300

    Add example result request to index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,5 @@
 <template>
   <section>
-    <pre>token: {{ token }}</pre>
-    <pre>me: {{ me }}</pre>
     <pre>result: {{ result }}</pre>
   </section>
 </template>
@@ -11,27 +9,15 @@ import Vue from 'vue'
 
 export default Vue.extend({
   name: 'IndexPage',
-  data: () => ({
-    token: window.localStorage.getItem('access_token'),
-  }),
 
   async asyncData({ $axios }) {
-    const me = await $axios.$get('/me', {
-      headers: {
-        Authorization: `Bearer ${window.localStorage.getItem('access_token')}`,
-      },
-    })
-
-    const result = await $axios.$get('/result', {
+    const result = await $axios.$get('/api/result', {
       params: {
         studentID: 304,
       },
-      headers: {
-        Authorization: `Bearer ${window.localStorage.getItem('access_token')}`,
-      },
     })
 
-    return { me, result }
+    return { result }
   },
 })
 </script>
