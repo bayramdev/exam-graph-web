a6e814931a008172da4c51fa4d097db690e8ead1
bayramdev <48856944+bayramdev@users.noreply.github.com>
2022-04-27 22:53:53 +0300

    Set up nuxt auth proxy

--- nuxt.config.js
+++ nuxt.config.js
@@ -39,18 +39,30 @@ export default {
   modules: [
     // https://go.nuxtjs.dev/axios
     '@nuxtjs/axios',
+    '@nuxtjs/auth-next',
+    '@nuxtjs/proxy',
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
-    // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: process.env.EXAM_GRAPH_API_BASE_URL || 'http://localhost:3000',
+    proxy: true,
+  },
+
+  proxy: {
+    '/api/': {
+      target: process.env.EXAM_GRAPH_API_BASE_URL,
+      pathRewrite: { '^/api/': '' },
+    },
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
 
-  env: {
-    baseURL: process.env.EXAM_GRAPH_API_BASE_URL || 'http://localhost:3000',
+  router: {
+    middleware: ['auth'],
+  },
+
+  compilerOptions: {
+    types: ['@nuxtjs/auth-next'],
   },
 }
