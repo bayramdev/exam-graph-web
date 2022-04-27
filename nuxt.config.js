a795bdea5a8da24a9cc88a721bcdb059cc3d785b
bayramdev <48856944+bayramdev@users.noreply.github.com>
2022-04-27 17:50:57 +0300

    Set up axios baseURL

--- nuxt.config.js
+++ nuxt.config.js
@@ -44,7 +44,7 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: '/',
+    baseURL: process.env.EXAM_GRAPH_API_BASE_URL || 'http://localhost:3000',
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
